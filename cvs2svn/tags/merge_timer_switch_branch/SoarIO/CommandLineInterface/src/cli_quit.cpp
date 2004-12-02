cdc13f6ce4872c0c9f033c45dee074fc0f500802
nobody <nobody@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2004-12-02 18:55:32 +0000

    This commit was manufactured by cvs2svn to create tag 'merge_timer_switch_branch'.

---
+++ cvs2svn/tags/merge_timer_switch_branch/SoarIO/CommandLineInterface/src/cli_quit.cpp
@@ -0,0 +1,25 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif // HAVE_CONFIG_H
+
+#include "cli_CommandLineInterface.h"
+
+using namespace cli;
+
+bool CommandLineInterface::ParseQuit(gSKI::IAgent* pAgent, std::vector<std::string>& argv) {
+	unused(pAgent);
+
+	// Quit needs no help
+	argv.clear();
+	return DoQuit();
+}
+
+bool CommandLineInterface::DoQuit() {
+	// Simply flip the quit flag
+	m_QuitCalled = true; 
+
+	// Toodles!
+	AppendToResult("Goodbye.");
+	return true;
+}
+
