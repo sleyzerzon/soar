3c4d7c4143666faee9ad6b0f2646973bc08ef69c
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2005-05-17 19:26:19 +0000

    merged with head

--- cvs2svn/branches/linux_platform_changes/SoarJavaDebugger/buildDebugger.bat
+++ cvs2svn/branches/linux_platform_changes/SoarJavaDebugger/buildDebugger.bat
@@ -1,10 +1,5 @@
 @REM Builds the debugger
 
-set SOARIO=..\soario
-set SOARLIB=..\soar-library
-
-xcopy %SOARIO%\ClientSMLSWIG\java\build\*.java sml\*.java /y
-
-javac.exe -classpath .;swt.jar debugger\Application.java
+javac.exe -classpath .;..\soar-library\swt.jar;..\soar-library\sml.jar debugger\Application.java
 jar.exe cvfm ..\soar-library\SoarJavaDebugger.jar JarManifest .
 pause
