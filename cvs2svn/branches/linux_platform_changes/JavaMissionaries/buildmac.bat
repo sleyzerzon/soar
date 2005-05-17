e292040527ad546ff810a0f8ba85aeb78c01b580
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2005-05-17 20:42:57 +0000

    head merge

--- cvs2svn/branches/linux_platform_changes/JavaMissionaries/buildmac.bat
+++ cvs2svn/branches/linux_platform_changes/JavaMissionaries/buildmac.bat
@@ -1,5 +1,4 @@
-xcopy ..\SoarIO\ClientSMLSWIG\Java\build\*.java src\sml /Y /I
-javac -classpath .;..\soar-library\swt.jar -sourcepath src\sml;src src\edu\umich\mac\MissionariesAndCannibals.java
+javac -classpath .;..\soar-library\swt.jar;..\soar-library\sml.jar -sourcepath src src\edu\umich\mac\MissionariesAndCannibals.java
 jar cvfm ..\soar-library\mac.jar macJarManifest mac -C src .
 xcopy mac\mac.soar ..\soar-library\mac\* /Y
 pause
