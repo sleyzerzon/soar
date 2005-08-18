de33bdd62a39ba023280cbaf57f52a3613b45435
rmarinie <rmarinie@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2005-08-18 20:30:52 +0000

    initial commit.  Not yet functional.

---
+++ cvs2svn/trunk/SoarIO/examples/TestSoarPerformance/TestSoarPerformance.cpp
@@ -0,0 +1,80 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif // HAVE_CONFIG_H
+
+// Use Visual C++'s memory checking functionality
+#ifdef _MSC_VER
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif // _MSC_VER
+
+#include <string>
+#include <iostream>
+#include "sml_Client.h"
+#include "sml_Connection.h"
+
+using namespace std;
+using namespace sml;
+
+void MyPrintEventHandler(smlPrintEventId id, void* pUserData, Agent* pAgent, char const* pMessage) {
+	cout << pMessage << endl;
+}
+
+void main() {
+
+	// When we have a memory leak, set this variable to
+	// the allocation number (e.g. 122) and then we'll break
+	// when that allocation occurs.
+	//_crtBreakAlloc = 73 ;
+
+	{ // create local scope to allow for local memory cleanup before we check at end
+		
+		//
+		// set up Soar
+        //
+		Kernel* kernel = Kernel::CreateKernelInNewThread("SoarKernelSML");
+		Agent* agent = kernel->CreateAgent("Soar1");
+		
+		agent->RegisterForPrintEvent(smlEVENT_PRINT, MyPrintEventHandler, NULL);
+
+		agent->ExecuteCommandLine("rete-net -l test.soarx");
+		agent->ExecuteCommandLine("watch 0");
+		agent->ExecuteCommandLine("max-elaborations 5000");
+		AnalyzeXML response;
+		agent->ExecuteCommandLineXML("time run", &response);
+		
+		cout << endl << response.GenerateXMLString(true) << endl;
+		//string time = response.GetResultString();
+		agent->ExecuteCommandLineXML("stats", &response);
+		//cout << endl << "Time: " << time << endl;
+		cout << endl << response.GenerateXMLString(true) << endl;
+		
+		kernel->Shutdown();
+		delete kernel;
+
+		//
+		// Show results
+		//
+		
+
+		string dummy;
+		cout << endl << endl << "Press any key and enter to continue";
+		cin >> dummy;
+
+	} // end local scope
+
+#ifdef _MSC_VER
+	// Set the memory checking output to go to Visual Studio's debug window (so we have a copy to keep)
+	// and to stdout so we can see it immediately.
+	_CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG );
+	_CrtSetReportFile( _CRT_WARN, _CRTDBG_FILE_STDOUT );
+
+	// Now check for memory leaks.
+	// This will only detect leaks in objects that we allocate within this executable and static libs.
+	// If we allocate something in a DLL then this call won't see it because it works by overriding the
+	// local implementation of malloc.
+	_CrtDumpMemoryLeaks();
+#endif // _MSC_VER
+	
+}
