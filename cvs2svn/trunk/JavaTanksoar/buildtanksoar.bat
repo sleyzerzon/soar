a0f7f93d3237981901b01ce63a8d7f40ad04f4cc
calmofthestorm <calmofthestorm@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2005-09-08 20:14:20 +0000

    *** empty log message ***

---
+++ cvs2svn/trunk/JavaTanksoar/buildtanksoar.bat
@@ -0,0 +1,10 @@
+del /s /Q *.class
+
+javac -classpath ..\soar-library\swt-windows.jar;..\soar-library\sml.jar -sourcepath src src\edu\umich\tanksoar\TanksoarJ.java
+
+jar cfm ..\soar-library\tanksoar.jar tanksoarJarManifest images -C src .
+
+rem copy the images over to soar-library, because the code doesn't currently load them from the jar (even though they are in there)
+xcopy /I /Q /E /Y images ..\soar-library\images
+
+IF NOT "%1"=="--nopause" pause
