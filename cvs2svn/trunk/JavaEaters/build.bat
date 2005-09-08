5f2b90a2d059ce65d5a3f842843d3a3cf5cdf7b9
rmarinie <rmarinie@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2005-09-08 20:39:50 +0000

    switch to use more standard "JarManifest" instead of "eatersJarManifest"

--- cvs2svn/trunk/JavaEaters/build.bat
+++ cvs2svn/trunk/JavaEaters/build.bat
@@ -2,6 +2,6 @@ del /s /Q *.class
 
 javac -classpath ..\soar-library\swt-windows.jar;..\soar-library\sml.jar;..\soar-library\JavaBaseEnvironment.jar -sourcepath src src\edu\umich\eaters\Eaters.java
 
-jar cfm ..\soar-library\eaters.jar eatersJarManifest -C src .
+jar cfm ..\soar-library\eaters.jar JarManifest -C src .
 
 IF NOT "%1"=="--nopause" pause
