c3292ca61fe9b8b23bac46a86b9c38d0bd5a17cc
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2005-10-25 19:46:41 +0000

    build modified, stokes stank removed

--- cvs2svn/trunk/JavaTanksoar/build.bat
+++ cvs2svn/trunk/JavaTanksoar/build.bat
@@ -2,9 +2,6 @@ del /s /Q *.class
 
 javac -classpath ..\soar-library\swt-windows.jar;..\soar-library\sml.jar;..\soar-library\JavaBaseEnvironment.jar -sourcepath src src\edu\umich\tanksoar\TanksoarJ.java
 
-jar cfm ..\soar-library\tanksoar.jar JarManifest images -C src .
-
-rem copy the images over to soar-library, because the code doesn't currently load them from the jar (even though they are in there)
-xcopy /I /Q /E /Y images ..\soar-library\images
+jar cfm ..\soar-library\tanksoar.jar JarManifest -C src .
 
 IF NOT "%1"=="--nopause" pause
