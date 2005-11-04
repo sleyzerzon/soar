56698b5c6e6bf6c11fc7d8b0459e7f0b88d65e54
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2005-11-04 21:48:10 +0000

    update

--- cvs2svn/trunk/JavaEaters/build.sh
+++ cvs2svn/trunk/JavaEaters/build.sh
@@ -3,13 +3,13 @@ SOARLIB="../soar-library"
 
 if [[ `uname -s` == "Darwin" ]]
 then
-  if ! javac -classpath .:${SOARLIB}/swt-osx.jar:${SOARLIB}/sml.jar -sourcepath src src/edu/umich/eaters/Eaters.java; then
+  if ! javac -classpath .:${SOARLIB}/swt-osx.jar:${SOARLIB}/sml.jar:${SOARLIB}/JavaBaseEnvironment.jar -sourcepath src src/edu/umich/eaters/Eaters.java; then
     echo "Build failed."
     exit 1;
   fi
   jar cfm ${SOARLIB}/eaters.jar JarManifest-osx -C src .
 else
-  if ! javac -classpath .:${SOARLIB}/swt-motif.jar:${SOARLIB}/sml.jar -sourcepath src src/edu/umich/eaters/Eaters.java; then
+  if ! javac -classpath .:${SOARLIB}/swt-motif.jar:${SOARLIB}/sml.jar:${SOARLIB}/JavaBaseEnvironment.jar -sourcepath src src/edu/umich/eaters/Eaters.java; then
     echo "Build failed."
     exit 1;
   fi
--- cvs2svn/trunk/JavaTanksoar/build.sh
+++ cvs2svn/trunk/JavaTanksoar/build.sh
@@ -3,13 +3,13 @@ SOARLIB="../soar-library"
 
 if [[ `uname -s` == "Darwin" ]]
 then
-  if ! javac -classpath .:${SOARLIB}/swt-osx.jar:${SOARLIB}/sml.jar -sourcepath src src/edu/umich/tanksoar/TanksoarJ.java; then
+  if ! javac -classpath .:${SOARLIB}/swt-osx.jar:${SOARLIB}/sml.jar:${SOARLIB}/JavaBaseEnvironment.jar -sourcepath src src/edu/umich/tanksoar/TanksoarJ.java; then
     echo "Build failed."
     exit 1;
   fi
   jar cfm ${SOARLIB}/tanksoar.jar JarManifest-osx -C src .
 else
-  if ! javac -classpath .:${SOARLIB}/swt-motif.jar:${SOARLIB}/sml.jar -sourcepath src src/edu/umich/tanksoar/TanksoarJ.java; then
+  if ! javac -classpath .:${SOARLIB}/swt-motif.jar:${SOARLIB}/sml.jar:${SOARLIB}/JavaBaseEnvironment.jar -sourcepath src src/edu/umich/tanksoar/TanksoarJ.java; then
     echo "Build failed."
     exit 1;
   fi
