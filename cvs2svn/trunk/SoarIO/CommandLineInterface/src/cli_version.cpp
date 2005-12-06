f2a054af65bf0c15ad0b066ff27ec1b9b13a25c1
rmarinie <rmarinie@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2005-12-06 16:53:48 +0000

    replaced __TIMESTAMP__ macro with ANSI (i.e. cross-platform) code based on __TIME__ and __DATE__

--- cvs2svn/trunk/SoarIO/CommandLineInterface/src/cli_version.cpp
+++ cvs2svn/trunk/SoarIO/CommandLineInterface/src/cli_version.cpp
@@ -19,7 +19,8 @@
 using namespace cli;
 using namespace sml;
 
-const char* kTimestamp = __TIMESTAMP__;
+const char* kTimestamp = __TIME__;
+const char* kDatestamp = __DATE__;
 
 bool CommandLineInterface::ParseVersion(gSKI::IAgent* pAgent, std::vector<std::string>& argv) {
 	unused(pAgent);
@@ -32,15 +33,19 @@ bool CommandLineInterface::DoVersion() {
 
 	char buf[kMinBufferSize];
 
+	std::ostringstream timedatestamp;
+	timedatestamp << kDatestamp << " " << kTimestamp;
+	std::string sTimeDateStamp = timedatestamp.str();
+
 	if (m_RawOutput) {
 		m_Result << m_KernelVersion.major << '.' << m_KernelVersion.minor << '.' << m_KernelVersion.micro << '\n';
-		m_Result << "Build date: " << kTimestamp;
+		m_Result << "Build date: " << sTimeDateStamp.c_str() << " " ;
 
 	} else {
 		AppendArgTagFast(sml_Names::kParamVersionMajor, sml_Names::kTypeInt, Int2String(m_KernelVersion.major, buf, kMinBufferSize));
 		AppendArgTagFast(sml_Names::kParamVersionMinor, sml_Names::kTypeInt, Int2String(m_KernelVersion.minor, buf, kMinBufferSize));
 		AppendArgTagFast(sml_Names::kParamVersionMicro, sml_Names::kTypeInt, Int2String(m_KernelVersion.micro, buf, kMinBufferSize));
-		AppendArgTagFast(sml_Names::kParamBuildDate, sml_Names::kTypeString, kTimestamp);
+		AppendArgTag(sml_Names::kParamBuildDate, sml_Names::kTypeString, sTimeDateStamp.c_str());
 	}
 	return true;
 }
