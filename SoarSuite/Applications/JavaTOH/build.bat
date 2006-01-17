9a5cfb58341f0b5430dfc3ab2c35de9167f9461d
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2006-01-17 20:21:52 +0000

    Copied remotely

--- SoarSuite/Applications/JavaTOH/build.bat
+++
@@ -1,4 +0,0 @@
-del /S /Q *.class
-javac -classpath .;..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar -sourcepath src src\edu\umich\toh\TowersOfHanoi.java
-jar cfm ..\..\SoarLibrary\bin\toh.jar JarManifest -C src .
-IF NOT "%1"=="--nopause" pause
