ea7d5986bc744166b83590698dc7ebdd1342aded
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2006-01-17 20:21:20 +0000

    Copied remotely

--- SoarSuite/Applications/JavaTankSoar/build.bat
+++
@@ -1,7 +0,0 @@
-del /s /Q *.class
-
-javac -classpath ..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar;..\..\SoarLibrary\bin\JavaBaseEnvironment.jar -sourcepath src src\edu\umich\tanksoar\TanksoarJ.java
-
-jar cfm ..\..\SoarLibrary\bin\tanksoar.jar JarManifest -C src .
-
-IF NOT "%1"=="--nopause" pause
