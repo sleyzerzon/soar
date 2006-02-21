93a067968723c36483fb4192b7ba3d8ac5bbaa9e
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2006-02-21 23:31:46 +0000

    should have been added with last commit

---
+++ branches/voigtjr-JavaEnvironmentsChanges/SoarSuite/Environments/JavaEaters/source/eaters/visuals/CreateEaterDialog.java
@@ -0,0 +1,109 @@
+package eaters.visuals;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.widgets.*;
+
+import eaters.Eater;
+import eaters.EatersSimulation;
+import eaters.SimulationListener;
+
+import utilities.*;
+
+public class CreateEaterDialog extends Dialog {
+
+	EatersSimulation m_Simulation;
+	String m_Productions;
+	Label m_ProductionsLabel;
+	Text m_Name;
+	Combo m_Color;
+	
+	public CreateEaterDialog(Shell parent, EatersSimulation simulation) {
+		super(parent);
+		m_Simulation = simulation;
+	}
+	
+	public void open() {
+		Shell parent = getParent();
+		final Shell dialog = new Shell(parent, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+		dialog.setSize(300,100);
+		dialog.setText("Create Eater");
+		
+		GridLayout gl = new GridLayout();
+		gl.numColumns = 3;
+		dialog.setLayout(gl);
+
+		GridData gd;
+		
+		final Label label1 = new Label(dialog, SWT.NONE);
+		gd = new GridData();
+		gd.horizontalAlignment = GridData.BEGINNING;
+		label1.setLayoutData(gd);
+		label1.setText("Name:");
+		
+		m_Name = new Text(dialog, SWT.SINGLE | SWT.BORDER);
+		gd = new GridData();
+		gd.horizontalSpan = 2;
+		gd.widthHint = 150;
+		gd.grabExcessHorizontalSpace = true;
+		m_Name.setLayoutData(gd);
+		
+		final Label label2 = new Label(dialog, SWT.NONE);
+		gd = new GridData();
+		gd.horizontalAlignment = GridData.BEGINNING;
+		label2.setText("Productions:");
+		
+		m_ProductionsLabel = new Label(dialog, SWT.NONE);
+		gd = new GridData();
+		gd.grabExcessHorizontalSpace = true;
+		gd.widthHint = 150;
+		m_ProductionsLabel.setLayoutData(gd);
+		m_ProductionsLabel.setText("<choose productions>");
+
+		final Button productionsBrowse = new Button(dialog, SWT.PUSH);
+		gd = new GridData();
+		gd.horizontalAlignment = GridData.BEGINNING;
+		productionsBrowse.setLayoutData(gd);
+		productionsBrowse.setText("...");
+		productionsBrowse.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				FileDialog fd = new FileDialog(dialog, SWT.OPEN);
+				fd.setText("Open");
+				fd.setFilterPath(m_Simulation.getAgentPath());
+				m_Productions = fd.open();
+				m_ProductionsLabel.setText(m_Productions.substring(m_Productions.lastIndexOf(System.getProperty("file.separator")) + 1));
+			}
+		});
+		
+		final Label label4 = new Label(dialog, SWT.NONE);
+		gd = new GridData();
+		gd.horizontalAlignment = GridData.BEGINNING;
+		label4.setLayoutData(gd);
+		label4.setText("Color:");
+		
+		m_Color = new Combo(dialog, SWT.NONE);
+		gd = new GridData();
+		gd.horizontalSpan = 2;
+		m_Color.setLayoutData(gd);
+		m_Color.setItems(EatersWindowManager.kColors);
+		// remove taken colors
+		Eater[] eaters = m_Simulation.getWorld().getEaters();
+		for (int i = 0; i < eaters.length; ++i) {
+			m_Color.remove(eaters[i].getColorString());
+		}
+		m_Color.select(0);
+//		m_Color.addSelectionListener(new SelectionAdapter() {
+//			public void widgetSelected(SelectionEvent e) {
+//			}
+//		});
+		
+		dialog.open();
+		Display display = parent.getDisplay();
+		while (!dialog.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+}
