52ef1da5d9446f77dd964bbf216321d1a2213ab7
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2006-02-27 18:29:17 +0000

    small bug fix so help uses string constant

--- branches/voigtjr-JavaEnvironmentsChanges/SoarSuite/Environments/JavaEaters/source/eaters/Eaters.java
+++ branches/voigtjr-JavaEnvironmentsChanges/SoarSuite/Environments/JavaEaters/source/eaters/Eaters.java
@@ -130,7 +130,7 @@ public class Eaters {
 	protected void printCommandLineHelp() {
 		System.out.println("Java Eaters help");
 		System.out.println("\t-console: Send all log messages to console, overridden by -log.");
-		System.out.println("\t-log: File name to log messages to (default: EaterLog.txt).");
+		System.out.println("\t-log: File name to log messages to (default: " + kDefaultFile + ").");
 		System.out.println("\t-append: If logging to file, append.  Ignored if -console present.");
 		System.out.println("\t-quiet: Disables all windows, runs simulation quietly.");
 		System.out.println("\t-settings: XML file with with run settings.");
