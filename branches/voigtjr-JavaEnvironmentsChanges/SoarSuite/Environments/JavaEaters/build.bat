62ae6dc9f9645d8b4d6d55ee16c7d7c2435f8a91
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2006-02-27 21:42:00 +0000

    updated build files and fixed some pathing issues

--- branches/voigtjr-JavaEnvironmentsChanges/SoarSuite/Environments/JavaEaters/build.bat
+++ branches/voigtjr-JavaEnvironmentsChanges/SoarSuite/Environments/JavaEaters/build.bat
@@ -1,4 +1,5 @@
 mkdir bin
+del /s /Q bin\*.class
 javac -d bin -classpath ..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar;..\..\SoarLibrary\bin\JavaBaseEnvironment.jar -sourcepath source source\eaters\Eaters.java
 xcopy /y source\* bin
 
