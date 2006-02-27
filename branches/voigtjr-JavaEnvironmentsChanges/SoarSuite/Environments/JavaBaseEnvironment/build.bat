e8515f6c45259c4c7019a8eb21ed28e026031fb6
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2006-02-27 21:41:59 +0000

    updated build files and fixed some pathing issues

--- branches/voigtjr-JavaEnvironmentsChanges/SoarSuite/Environments/JavaBaseEnvironment/build.bat
+++ branches/voigtjr-JavaEnvironmentsChanges/SoarSuite/Environments/JavaBaseEnvironment/build.bat
@@ -1,5 +1,6 @@
 mkdir bin
-javac -d bin -classpath ..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar -sourcepath source source\utilities\*.java
+del /s /Q bin\*.class
+javac -d bin -classpath ..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar -sourcepath source source\utilities\*.java source\simulation\*.java
 
 jar cf ..\..\SoarLibrary\bin\JavaBaseEnvironment.jar -C bin .
 
