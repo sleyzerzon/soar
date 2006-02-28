d067b60e85ff735596f828c348eb99f4c3b6d575
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2006-02-28 21:42:34 +0000

    added visuals to build path

--- branches/voigtjr-JavaEnvironmentsChanges/SoarSuite/Environments/JavaBaseEnvironment/build.bat
+++ branches/voigtjr-JavaEnvironmentsChanges/SoarSuite/Environments/JavaBaseEnvironment/build.bat
@@ -1,6 +1,6 @@
 mkdir bin
 del /s /Q bin\*.class
-javac -d bin -classpath ..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar -sourcepath source source\utilities\*.java source\simulation\*.java
+javac -d bin -classpath ..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar -sourcepath source source\utilities\*.java source\simulation\*.java source\simulation\visuals\*.java
 
 jar cf ..\..\SoarLibrary\bin\JavaBaseEnvironment.jar -C bin .
 
