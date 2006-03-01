50d37282df94ab56b94a158a368538cabd0bfd94
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2006-03-01 00:44:22 +0000

    updated to copy over images

--- branches/voigtjr-JavaEnvironmentsChanges/SoarSuite/Environments/JavaTankSoar/build.bat
+++ branches/voigtjr-JavaEnvironmentsChanges/SoarSuite/Environments/JavaTankSoar/build.bat
@@ -1,6 +1,8 @@
 mkdir bin
 javac -d bin -classpath ..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar;..\..\SoarLibrary\bin\JavaBaseEnvironment.jar -sourcepath source source\eaters\TankSoar.java
 xcopy /y source\* bin
+mkdir bin\images
+xcopy /y /s source\images\* bin\images
 
 jar cfm ..\..\SoarLibrary\bin\JavaTankSoar.jar JarManifest -C bin .
 
