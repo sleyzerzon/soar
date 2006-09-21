77a5c904b56810ed3db86334e41b70f10a1cde82
anuxoll <anuxoll@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2006-09-21 15:44:43 +0000

    Added files from trunk rev 6084

--- branches/EpMem-8.6.2/Environments/JavaTankSoar/build.sh
+++ branches/EpMem-8.6.2/Environments/JavaTankSoar/build.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
+
+pushd `echo $0 | sed -n 's/^\(.*\)build.sh/\1/p'` > /dev/null
+
 SOARLIB="../../SoarLibrary/bin"
 
 if [ ! -e bin ]; then
         mkdir bin
 	mkdir bin/images
 else
-        for file in `find bin -iname *.class`
+        for file in `find bin -iname "*.class"`
         do
                 rm $file
         done
-        for file in `find bin -iname *.xml`
+        for file in `find bin -iname "*.xml"`
         do
                 rm $file
         done
@@ -19,13 +22,15 @@ else
 	mkdir bin/images
 fi
 
-if ! javac -source 1.4 -d bin -classpath ${SOARLIB}/swt.jar:${SOARLIB}/sml.jar:${SOARLIB}/JavaBaseEnvironment.jar -sourcepath source source/tanksoar/TankSoar.java ; then
-	echo "Build failed."
-	exit 1;
+javac -source 1.4 -d bin -classpath ${SOARLIB}/swt.jar:${SOARLIB}/sml.jar:${SOARLIB}/JavaBaseEnvironment.jar -sourcepath source source/tanksoar/TankSoar.java
+RET=$?
+if [[ $RET = 0 ]]
+  then cp -f source/*.xml bin
+  cp -f source/images/* bin/images
+  jar cfm JavaTankSoar.jar JarManifest -C bin .
+  RET=$?
 fi
 
-cp -f source/*.xml bin
-cp -f source/images/* bin/images
+popd > /dev/null
 
-jar cfm JavaTankSoar.jar JarManifest -C bin .
-
+exit $RET
