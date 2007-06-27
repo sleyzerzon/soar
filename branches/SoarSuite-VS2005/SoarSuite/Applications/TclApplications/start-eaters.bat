4a51a420eafde9b1bdb419fedc8f3e491efcadb4
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2007-06-27 17:25:28 +0000

    Removed old branch

--- branches/SoarSuite-VS2005/SoarSuite/Applications/TclApplications/start-eaters.bat
+++
@@ -1,3 +0,0 @@
-cd ..\..\SoarLibrary\bin
-start wish ..\..\Applications\TclApplications\eaters\init-eaters.tcl
-cd ..\..\Applications\TclApplications
--- branches/SoarSuite-VS2005/SoarSuite/Applications/TestJavaSML/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# *** MAKE SURE CHANGES TO THIS FILE ARE REFLECTED IN THE .BAT FILE
-
-SOARLIB=../../SoarLibrary/bin
-
-javac -classpath .:${SOARLIB}/swt.jar:${SOARLIB}/sml.jar -sourcepath . Application.java
-jar cfm ${SOARLIB}/TestJavaSML.jar JarManifest .
