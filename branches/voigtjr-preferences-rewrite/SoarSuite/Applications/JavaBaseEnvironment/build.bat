fd34afcbe946895531972ab8e109ed93bdd40097
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2007-06-27 17:24:50 +0000

    Removed old branch

--- branches/voigtjr-preferences-rewrite/SoarSuite/Applications/JavaBaseEnvironment/build.bat
+++
@@ -1,7 +0,0 @@
-del /s /Q *.class
-
-javac -classpath ..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar -sourcepath src src\edu\umich\JavaBaseEnvironment\*.java
-
-jar cf ..\..\SoarLibrary\bin\javabaseenvironment.jar -C src .
-
-IF NOT "%1"=="--nopause" pause
