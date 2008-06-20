b1191907f4cd8ba97c0cab52c0157dcacd76797b
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2008-06-20 16:45:19 +0000

    Removed file/folder

--- tags/Soar-8-6-1-r4/SoarIO/examples/TestJavaSML/buildJava.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# *** MAKE SURE CHANGES TO THIS FILE ARE REFLECTED IN THE .BAT FILE
-
-SOARLIB=../../../soar-library
-
-javac -classpath .:${SOARLIB}/swt.jar:${SOARLIB}/sml.jar Application.java
-jar cvfm ${SOARLIB}/TestJavaSML.jar JarManifest .
--- tags/Soar-8-6-1-r4/tclenvironments/start-eaters.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd ../soar-library
-wish ../tclenvironments/eaters/init-eaters.tcl
