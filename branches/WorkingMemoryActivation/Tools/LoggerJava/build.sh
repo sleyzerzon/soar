422d649c86f6c6ddbf3b09b5d5557163d9d71a9e
nlderbin <nlderbin@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2008-08-20 16:00:23 +0000

    replaced by nlderbin-trunk-wma

--- branches/WorkingMemoryActivation/Tools/LoggerJava/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# *** MAKE SURE CHANGES TO THIS FILE ARE REFLECTED IN THE .BAT FILE
-
-SOARLIB=../../SoarLibrary/bin
-
-javac -classpath .:${SOARLIB}/swt.jar:${SOARLIB}/sml.jar -sourcepath . log/MainFrame.java
-jar cfm ${SOARLIB}/LoggerJava.jar JarManifest .
