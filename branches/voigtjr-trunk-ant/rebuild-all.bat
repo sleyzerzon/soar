bb7a9b381841bae93798b1287b96799c33117003
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2009-03-31 19:03:17 +0000

    removed obsolete build files, updated rebuild-all to use ant

--- branches/voigtjr-trunk-ant/rebuild-all.bat
+++ branches/voigtjr-trunk-ant/rebuild-all.bat
@@ -1,9 +1,5 @@
 @echo off
 
-rem %1 is an optional command line argument specifying the java bin directory to use to build everything
-rem NOTE: if this directory is specified, the trailing slash must be included!
-rem Also, if there are spaces in the path, it must be wrapped in quotes
-
 @rem Step 1: Set up build environment
 call "%VS80COMNTOOLS%\vsvars32.bat"
 
@@ -14,8 +10,9 @@ devenv /build Release Tools\TestCSharpSML\TestCSharpSML.sln
 if not errorlevel 0 goto fail
 
 @rem Step 3: Build Java
-call buildJavaApps.bat %1
+ant
 if not errorlevel 0 goto fail
+
 exit 0
 
 :fail
