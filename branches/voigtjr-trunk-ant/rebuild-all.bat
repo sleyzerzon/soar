daa3c43ed462438327ddebc23abec8d822e0e687
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2009-03-31 21:27:03 +0000

    added ant clean step, removed old java call

--- branches/voigtjr-trunk-ant/rebuild-all.bat
+++ branches/voigtjr-trunk-ant/rebuild-all.bat
@@ -1,19 +1,14 @@
 @echo off
 
-@rem Step 1: Set up build environment
 call "%VS80COMNTOOLS%\vsvars32.bat"
 
-@rem Step 2: Build C++
+call %ANT_HOME%\bin\ant clean
+
 devenv /rebuild "Distribution SCU" SML.sln
 if not errorlevel 0 goto fail
+
 devenv /build Release Tools\TestCSharpSML\TestCSharpSML.sln
 if not errorlevel 0 goto fail
-
-@rem Step 3: Clean and Build Java
-%ANT_HOME%\bin\ant clean
-%ANT_HOME%\bin\ant
-if not errorlevel 0 goto fail
-
 exit 0
 
 :fail
