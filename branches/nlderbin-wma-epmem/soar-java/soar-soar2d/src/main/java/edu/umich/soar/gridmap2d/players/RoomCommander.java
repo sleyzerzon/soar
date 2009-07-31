6ca598d4ddcbec20310c3778ef483bc779ba7be2
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2009-07-31 04:37:41 +0000

    merged from trunk 10913

---
+++ branches/nlderbin-wma-epmem/soar-java/soar-soar2d/src/main/java/edu/umich/soar/gridmap2d/players/RoomCommander.java
@@ -0,0 +1,11 @@
+package edu.umich.soar.gridmap2d.players;
+
+import java.util.List;
+
+import edu.umich.soar.gridmap2d.map.RoomMap;
+
+public interface RoomCommander extends Commander {
+	public void update(RoomMap roomMap) throws Exception;
+	public void receiveMessage(Player player, List<String> message);
+	public List<double[]> getWaypointList();
+}
