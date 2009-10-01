391426f2aceb2ea696a0266387ad3558e3ece114
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2009-10-01 15:20:06 +0000

    Made set-velocity a bit more flexible, it now assumes zero for linear or angular if it isn't passed.

--- branches/nlderbin-epmem-smem/soar-java/soar-smljava/src/main/java/edu/umich/soar/robot/SetVelocityCommand.java
+++ branches/nlderbin-epmem-smem/soar-java/soar-smljava/src/main/java/edu/umich/soar/robot/SetVelocityCommand.java
@@ -12,7 +12,7 @@ import sml.Identifier;
  *
  * Set linear and angular velocities.
  * 
- * Returns executing. Not interruptable. Creates DDC.
+ * Returns executing. Not interruptible. Creates DDC.
  */
 final public class SetVelocityCommand extends DDCCommand implements Command {
 	private static final Logger logger = Logger.getLogger(SetVelocityCommand.class);
@@ -40,27 +40,33 @@ final public class SetVelocityCommand extends DDCCommand implements Command {
 		if (this.command != null) {
 			throw new IllegalStateException();
 		}
+
+		String linvelString = command.GetParameterValue(LINVEL);
+		String angvelString = command.GetParameterValue(ANGVEL);
 		
-		try {
-			linearVelocity = Double.parseDouble(command.GetParameterValue(LINVEL));
-		} catch (NullPointerException ex) {
-			CommandStatus.error.addStatus(command, NAME + ": No " + LINVEL + " on command");
-			return false;
-		} catch (NumberFormatException e) {
-			CommandStatus.error.addStatus(command, NAME + ": Unable to parse " + LINVEL + ": " + command.GetParameterValue(LINVEL));
+		if (linvelString == null && angvelString == null) {
+			CommandStatus.error.addStatus(command, NAME + ": Must have at least one of " + LINVEL + " or " + ANGVEL + " on the command.");
 			return false;
 		}
 
-		try {
-			angularVelocity = Math.toRadians(Double.parseDouble(command.GetParameterValue(ANGVEL)));
-		} catch (NullPointerException ex) {
-			CommandStatus.error.addStatus(command, NAME + ": No " + ANGVEL + " on command");
-			return false;
-		} catch (NumberFormatException e) {
-			CommandStatus.error.addStatus(command, NAME + ": Unable to parse " + ANGVEL + ": " + command.GetParameterValue(ANGVEL));
-			return false;
+		if (linvelString != null) {
+			try {
+				linearVelocity = Double.parseDouble(linvelString);
+			} catch (NumberFormatException e) {
+				CommandStatus.error.addStatus(command, NAME + ": Unable to parse " + LINVEL + ": " + command.GetParameterValue(LINVEL));
+				return false;
+			}
 		}
-
+		
+		if (angvelString != null) {
+			try {
+				angularVelocity = Math.toRadians(Double.parseDouble(angvelString));
+			} catch (NumberFormatException e) {
+				CommandStatus.error.addStatus(command, NAME + ": Unable to parse " + ANGVEL + ": " + command.GetParameterValue(ANGVEL));
+				return false;
+			}
+		}
+		
 		logger.debug(String.format(NAME + ": l%1.3f a%3.1f", linearVelocity, angularVelocity));
 		CommandStatus.accepted.addStatus(command);
 		CommandStatus.executing.addStatus(command);
