c1ec6858a5ba96e9037820b60881a20849809962
voigtjr <voigtjr@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2009-10-09 19:15:47 +0000

    Merged branch to smem

--- branches/voigtjr-smem-timers/soar-java/soar-room/src/main/java/edu/umich/soar/gridmap2d/config/SoarConfig.java
+++
@@ -1,9 +0,0 @@
-package edu.umich.soar.gridmap2d.config;
-
-public class SoarConfig {
-	public int max_memory_usage = -1;
-	public int port = 12121;
-	public String remote = null;
-	public boolean spawn_debuggers = true;
-	public boolean soar_print = false;
-}
