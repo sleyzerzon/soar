3d888f72a7974d7a38d08f42481787413921f46e
josephzxu@gmail.com <josephzxu@gmail.com@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2011-06-29 19:53:26 +0000

    test vectorization speed-up

--- branches/SVS2/Core/SVS/tests/lwrtest.cpp
+++ branches/SVS2/Core/SVS/tests/lwrtest.cpp
@@ -12,25 +12,39 @@
 
 using namespace std;
 
-void split(const string &s, const string &delims, vector<string> &result) {
-	int i = 0, j = 0;
-	while ((j = s.find_first_of(delims, i)) != string::npos) {
-		result.push_back(s.substr(i, j - i));
-		i = j + 1;
-	}
-	result.push_back(s.substr(i));
-}
-
 void parsefloats(const string &s, vector<float> &v) {
 	stringstream ss(s);
-	double f;
-	
-	while (ss) {
+	float f;
+	int c = 1;
+	while (!ss.eof()) {
 		if (!(ss >> f)) {
-			break;
+			if (ss.eof()) {
+				break;
+			}
+			cerr << "error parsing field " << c << " in " << ss.str() << endl;
+			exit(1);
 		}
+
 		v.push_back(f);
+		c++;
 	}
+}
+
+void parseline(const string &line, floatvec &x, floatvec &y, float &dt) {
+	vector<string> parts;
+	vector<float> x1, y1;
+	
+	split(line, ";", parts);
+	assert(parts.size() == 3);
+	parsefloats(parts[0], x1);
+	parsefloats(parts[1], y1);
+	stringstream ss(parts[2]);
+	if (!(ss >> dt)) {
+		cerr << "error parsing " << parts[2] << endl;
+		exit(1);
+	}
+	x = x1;
+	y = y1;
 }
 
 int main(int argc, char *argv[]) {
@@ -38,10 +52,9 @@ int main(int argc, char *argv[]) {
 	int nnbrs;
 	lwr *m = NULL;
 	vector<double> diffs;
-	bool learning = true;
 	
-	if (argc != 4) {
-		cerr << "usage: lwrtest <method> <num neighbors> <data file>" << endl;
+	if (argc != 5) {
+		cerr << "usage: lwrtest <method> <num neighbors> <training file> <test file>" << endl;
 		exit(1);
 	}
 	
@@ -51,42 +64,46 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 	
-	ifstream input(argv[3]);
-	if (!input) {
+	ifstream trainf(argv[3]);
+	if (!trainf) {
 		cerr << "couldn't open " << argv[3] << endl;
 		exit(1);
 	}
 	
-	while (true) {
-		vector<string> parts;
-		
-		if (!getline(input, line)) {
-			break;
+	while (getline(trainf, line)) {
+		floatvec x, y;
+		float dt;
+		parseline(line, x, y, dt);
+		if (!m) {
+			m = new lwr(x.size(), y.size(), nnbrs);
 		}
-		
-		if (line[0] == '$') {
-			learning = false;
-			continue;
+		for(int i = 0; i < y.size(); ++i) {
+			y[i] = (y[i] - x[i]) / dt;
 		}
-		
-		split(line, ";", parts);
-		assert(parts.size() == 2);
-		
-		vector<float> x1, y1;
-		parsefloats(parts[0], x1);
-		parsefloats(parts[1], y1);
-		floatvec x2(x1), y2(y1);
-		
-		if (learning) {
-			if (!m) {
-				m = new lwr(x2.size(), y2.size(), nnbrs);
-			}
-			m->add(x2, y2);
-		} else {
-			floatvec py(y2.size());
-			m->predict(x2, py, argv[1][0], false);
-			diffs.push_back(sqrt(py.distsq(y2)));
+		m->add(x, y);
+	}
+	
+	trainf.close();
+	
+	ifstream testf(argv[4]);
+	if (!testf) {
+		cerr << "couldn't open " << argv[4] << endl;
+		exit(1);
+	}
+	
+	while (getline(testf, line)) {
+		floatvec x, y, dy, py;
+		float dt;
+		parseline(line, x, y, dt);
+		dy.resize(y.size());
+		py.resize(y.size());
+		m->predict(x, dy, argv[1][0], false);
+		for(int i = 0; i < py.size(); ++i) {
+			py[i] = x[i] + dy[i] * dt;
 		}
+		cout << "predict: " << py << endl;
+		cout << "actual:  " << y << endl;
+		diffs.push_back(sqrt(py.distsq(y)));
 	}
 	
 	if (diffs.size() == 0) {
