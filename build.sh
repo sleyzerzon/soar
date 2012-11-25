2d95d8480085e0851b2799f90157d333a7fe1abe
justinnhli <justinnhli@gmail.com>
2012-11-25 16:01:16 +0000

    modularized build script

--- build.sh
+++ build.sh
@@ -3,6 +3,68 @@
 soardir="$( cd ~/SoarSuite/ && pwd)"
 basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+clean() {
+	pushd april/java && ant clean && popd
+	pushd SoarRobotLibrary && ant clean && popd
+	pushd libgrrc-java && ant clean && popd
+	pushd SoarRobotServer && ant clean && popd
+}
+
+april() {
+	# build april
+	pushd april/java
+	ant
+	mv april.jar "$basedir/SoarRobotServer/lib/"
+	popd
+}
+
+soar-robot-library() {
+	# build SoarRobotLibrary 
+	pushd SoarRobotLibrary
+	ant
+	mv build/jar/SoarRobotLibrary.jar "$basedir/SoarRobotServer/lib/"
+	popd
+}
+
+grrc() {
+	# build libgrrc-java
+	pushd libgrrc-java
+	ant
+	mv build/jar/libgrrc-java.jar "$basedir/SoarRobotServer/lib/"
+	popd
+}
+
+copy-soar-libraries() {
+	# copy soar libraries
+	uname="$(uname)"
+	case "$uname" in
+		"Linux")
+			export arch="linux_32"
+			cp -f "$soardir/out/libJava_sml_ClientInterface.so" "native/$arch/"
+			cp -f "$soardir/out/libSoar.so" "native/$arch/";;
+		"Darwin")
+			export arch="osx"
+			cp -f "$soardir/out/libJava_sml_ClientInterface.jnilib" "native/$arch/"
+			cp -f "$soardir/out/libSoar.dylib" "native/$arch/";;
+		*)
+			echo "unknown architecture: $uname"
+			exit 1;;
+	esac
+	# where is TestExternalLibrary?;;
+	cp -f "$soardir/out/java/sml.jar" "native/$arch/"
+	cp -f "$soardir/out/java/soar-debugger-api.jar" "native/$arch/"
+	cp -f "$soardir/out/java/soar-debugger.jar" "native/$arch/"
+	cp -f "$soardir/out/java/soar-smljava.jar" "native/$arch/"
+	cp -f "$soardir/out/java/swt.jar" "native/$arch/"
+}
+
+soar-robot-server() {
+	# build SoarRobotServer
+	pushd SoarRobotServer
+	ant
+	popd
+}
+
 if [ ! $soardir ]; then
 	echo "Please modify script to use the correct path to Soar"
 	exit 1
@@ -11,54 +73,22 @@ fi
 # navigate to RoomsWorld base
 cd "$basedir"
 
-if [ "$1" = "clean" ]; then
-	pushd april/java && ant clean && popd
-	pushd SoarRobotLibrary && ant clean && popd
-	pushd libgrrc-java && ant clean && popd
-	pushd SoarRobotServer && ant clean && popd
-	exit
-fi
-
-# build april
-pushd april/java
-ant
-mv april.jar "$basedir/SoarRobotServer/lib/"
-popd
-
-# build SoarRobotLibrary 
-pushd SoarRobotLibrary
-ant
-mv build/jar/SoarRobotLibrary.jar "$basedir/SoarRobotServer/lib/"
-popd
-
-# build libgrrc-java
-pushd libgrrc-java
-ant
-mv build/jar/libgrrc-java.jar "$basedir/SoarRobotServer/lib/"
-popd
-
-# copy soar libraries
-uname="$(uname)"
-case "$uname" in
-	"Linux")
-		export arch="linux_32"
-		cp -f "$soardir/out/libJava_sml_ClientInterface.so" "native/$arch/"
-		cp -f "$soardir/out/libSoar.so" "native/$arch/";;
-	"Darwin")
-		export arch="osx"
-		cp -f "$soardir/out/libJava_sml_ClientInterface.jnilib" "native/$arch/"
-		cp -f "$soardir/out/libSoar.dylib" "native/$arch/";;
+case "$1" in
+	"clean")
+		clean
+		exit;;
+	"april")
+		april;;
+	"SoarRobotLibrary")
+		soar-robot-library;;
+	"grrc")
+		grrc;;
+	"all")
+		april
+		soar-robot-library
+		grrc
+		copy-soar-libraries
+		soar-robot-server;;
 	*)
-		echo "unknown architecture: $uname"
-		exit 1;;
+		soar-robot-server;;
 esac
-# where is TestExternalLibrary?;;
-cp -f "$soardir/out/java/sml.jar" "native/$arch/"
-cp -f "$soardir/out/java/soar-debugger-api.jar" "native/$arch/"
-cp -f "$soardir/out/java/soar-debugger.jar" "native/$arch/"
-cp -f "$soardir/out/java/soar-smljava.jar" "native/$arch/"
-cp -f "$soardir/out/java/swt.jar" "native/$arch/"
-
-# build
-cd SoarRobotServer
-ant
