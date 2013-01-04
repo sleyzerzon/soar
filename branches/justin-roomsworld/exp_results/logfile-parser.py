b6ea30f4274826a3d88956001d2ef6fedc17272d
justinnhli@gmail.com <justinnhli@gmail.com@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2013-01-04 20:23:02 +0000

    bugfixes and logfile parser

--- branches/justin-roomsworld/exp_results/logfile-parser.py
+++ branches/justin-roomsworld/exp_results/logfile-parser.py
@@ -4,36 +4,70 @@ import re
 import sys
 
 if __name__ == "__main__":
-	with open(sys.argv[1]) as fd:
-		echo, commands = fd.read().partition("END OUTPUT")[0].partition("BEGIN COMMANDS")[0:3:2]
-		"""
-		for line in echo.splitlines():
-			if re.match("[^>]*:", line):
-				print(line)
-				"""
-		sstats, stimer, estats, etimer, stats = commands.split("\n\n", 5)[1:]
+	for f in sys.argv[1:]:
+		with open(f, "r") as fd:
+			output = {}
+			echo, commands = fd.read().partition("END OUTPUT")[0].partition("BEGIN COMMANDS")[0:3:2]
 
-		# smem stats
-		extract = ("Memory Usage", "Memory Highwater", "Retrieves", "Queries", "Stores", "Activation Updates", "Mirrors",)
-		for line in sstats.splitlines():
-			if line.split(":")[0].strip() in extract:
-				print("smem {}".format(line))
+			# echo'ed output
+			for line in echo.splitlines():
+				if re.match("[^>]*:", line):
+					output.update(("echoed {}".format(k), v) for k, v in [line.split(":", 1),])
 
-		# smem timer
-		for line in stimer.splitlines():
-			if not re.match("[a-z]*>", line):
-				print("smem_timer {}".format(line))
+			sstats, stimer, estats, etimer, wma, stats = commands.split("seek>")[1:-1]
 
-		# epmem stats
-		extract = ("Time", "Memory Usage", "Memory Highwater", "Retrievals", "Queries", "Nexts", "Prevs",)
-		for line in estats.splitlines():
-			if line.split(":")[0].strip() in extract:
-				print("epmem {}".format(line))
+			# smem stats
+			extract = ("Memory Usage", "Memory Highwater", "Retrieves", "Queries", "Stores", "Activation Updates", "Mirrors",)
+			for line in sstats.splitlines():
+				if ":" in line:
+					output.update(("sstats {}".format(k), v) for k, v in [line.split(":", 1),] if k in extract)
 
-		# epmem timer
-		extract = ("_total", "epmem_ncb_retrieval", "epmem_next", "epmem_prev", "epmem_query", "epmem_storage",)
-		for line in etimer.splitlines():
-			if line.split(":")[0].strip() in extract:
-				print("epmem_timer {}".format(line))
+			# smem timer
+			extract = ("_total", "smem_api", "smem_hash", "smem_init", "smem_ncb_retrieval", "smem_query", "smem_storage",)
+			for line in stimer.splitlines():
+				if ":" in line:
+					output.update(("stimer {}".format(k), v) for k, v in [line.split(":", 1),] if k in extract)
 
-		# stats
+			# epmem stats
+			extract = ("Time", "Memory Usage", "Memory Highwater", "Retrievals", "Queries", "Nexts", "Prevs",)
+			for line in estats.splitlines():
+				if ":" in line:
+					output.update(("estats {}".format(k), v) for k, v in [line.split(":", 1),] if k in extract)
+
+			# epmem timer
+			extract = ("_total", "epmem_ncb_retrieval", "epmem_next", "epmem_prev", "epmem_query", "epmem_storage",)
+			for line in etimer.splitlines():
+				if ":" in line:
+					output.update(("etimer {}".format(k), v) for k, v in [line.split(":", 1),] if k in extract)
+
+			# wma
+			extract = ("decay-rate", "decay-thresh", "forgetting", "forget-wme",)
+			for line in wma.splitlines():
+				if ":" in line:
+					output.update(("wma {}".format(k), v) for k, v in [line.split(":", 1),] if k in extract)
+
+			# stats
+
+			# kernel CPU
+			output["stats kernel cpu sec"] = re.sub(".*Kernel CPU Time: *([0-9.]*).*", r"\1", stats, flags=re.DOTALL)
+
+			# total CPU
+			output["stats total cpu sec"] = re.sub(".*Total  CPU Time: *([0-9.]*).*", r"\1", stats, flags=re.DOTALL)
+
+			# decisions
+			output["stats decision"] = re.sub(".*?([0-9]+) decisions.*", r"\1", stats, flags=re.DOTALL)
+
+			# msec/decisions
+			output["stats decision msec"] = re.sub(".*?([0-9.]+) msec/decision.*", r"\1", stats, flags=re.DOTALL)
+
+			# elaboration cycles
+			output["stats elab cycles"] = re.sub(".*?([0-9]+) elaboration cycles.*", r"\1", stats, flags=re.DOTALL)
+
+			# avg wm size 
+			output["stats avg wm size"] = re.sub(".*?([0-9.]+) mean.*", r"\1", stats, flags=re.DOTALL)
+
+			# max wm size 
+			output["stats max wm size"] = re.sub(".*?([0-9.]+) maximum.*", r"\1", stats, flags=re.DOTALL)
+
+			print(" ".join(sorted("{}={}".format(re.sub("_+", "_", re.sub("[ -]", "_", k.lower())).strip("_"), v.strip()) for k, v in sorted(output.items()))))
+
