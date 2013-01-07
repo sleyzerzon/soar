7e019621afe7d72794201c7dc2c790480ac72f67
justinnhli <justinnhli@915bf4ee-6d5f-11de-9cf4-4bf9f06b8206>
2013-01-07 02:17:24 +0000

    randomly generated objects and tasks
    * changed config-generator to do the above
    * added parameters rand-seed, num-block, num-tasks, and tasks-file
    * removed static objects and tasks from config template and agent
    * added rule to load tasks soar file (from tasks-file parameter)
    * modified launch scripts to work from any directory
    * added epmem settings to logfile output

--- branches/justin-roomsworld/native/linux_64/RoomsWorld.sh
+++ branches/justin-roomsworld/native/linux_64/RoomsWorld.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-export LD_LIBRARY_PATH=`pwd`
-java -jar SoarRobotServer.jar --config "$1"
+
+cwd="$(pwd)"
+
+# if it's not an absolute path, insert the previous directory as prefix
+config="$1"
+if [ "${config:0:1}" != "/" ]; then
+	config="$cwd/$config"
+fi
+
+cd $(dirname "$0")
+export LD_LIBRARY_PATH="$(pwd)"
+java -jar SoarRobotServer.jar --config "$config"
