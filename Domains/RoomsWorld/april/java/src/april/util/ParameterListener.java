b2ebfa8a593e5671220cbc8318e8cdac466a8da1
mazzin <mazzin@gmail.com>
2013-01-29 22:44:12 +0000

    Last version between big change to epmem/smem schemas.  Also some UI changes.

--- Domains/RoomsWorld/april/java/src/april/util/ParameterListener.java
+++
@@ -1,9 +0,0 @@
-package april.util;
-
-/** Interface for receiving notifications when a parameter changes
- * (see ParameterGUI). **/
-public interface ParameterListener
-{
-    public void parameterChanged(ParameterGUI pg, String name);
-}
-
--- Domains/RoomsWorld/robot_lib/README.txt
+++
@@ -1,1 +0,0 @@
-These are jars that are part of the ROBOT group in the Eclipse projects.
